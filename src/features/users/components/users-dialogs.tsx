55f1b4fa4c9f998c3f4292f9cdd042b81313b089
urbanbboy <talant.04asanov@gmail.com>
2025-05-20 23:23:59 +0600

    fix errors

--- src/features/users/components/users-dialogs.tsx
+++ src/features/users/components/users-dialogs.tsx
@@ -1,7 +1,6 @@
 import { useUsers } from '../context/users-context'
 import { UsersActionDialog } from './users-action-dialog'
 import { UsersDeleteDialog } from './users-delete-dialog'
-import { UsersInviteDialog } from './users-invite-dialog'
 
 export function UsersDialogs() {
   const { open, setOpen, currentRow, setCurrentRow } = useUsers()
@@ -12,13 +11,6 @@ export function UsersDialogs() {
         open={open === 'add'}
         onOpenChange={() => setOpen('add')}
       />
-
-      <UsersInviteDialog
-        key='user-invite'
-        open={open === 'invite'}
-        onOpenChange={() => setOpen('invite')}
-      />
-
       {currentRow && (
         <>
           <UsersActionDialog
